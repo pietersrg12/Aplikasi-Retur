25dd51bf6d4e8b5f8d4fd8dbfed49500cde3f74a
pietersrg12 <pieter.saragih12@gmail.com>
2019-12-08 21:10:35 +0800

    layout Yantek

---
+++ app/src/main/java/id/co/aplikasiretur/YantekActivity.kt
@@ -0,0 +1,12 @@
+package id.co.aplikasiretur
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YantekActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yantek)
+    }
+}
